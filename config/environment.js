713c85e4f1e60cbc136370f13177254e1c8472de
ankit7542 <a121980k@gmail.com>
2022-09-30 10:55:08 +0530

    Added first project

---
+++ config/environment.js
@@ -0,0 +1,69 @@
+const fs = require('fs');
+const rfs = require('rotating-file-stream');
+const path = require('path');
+
+
+const logDirectory = path.join(__dirname, '../production_logs');
+fs.existsSync(logDirectory) || fs.mkdirSync(logDirectory);
+
+const accessLogStream = rfs('access.log', {
+    interval: '1d',
+    path: logDirectory
+});
+
+
+
+const development = {
+    name: 'development',
+    asset_path: '/assets',
+    session_cookie_key: 'blahsomething',
+    db: 'codeial_development',
+    smtp: {
+        service: 'gmail',
+        host: 'smtp.gmail.com',
+        port: 587,
+        secure: false,
+        auth: {
+            user: 'a121980k',
+            pass: 'Ankit@1996'
+        }
+    },
+    google_client_id: "699102155376-tjfa5pkcula0oq1rvom6dtme8pcs88ds.apps.googleusercontent.com",
+    google_client_secret: "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",
+    google_call_back_url: "http://localhost:8000/users/auth/google/callback",
+    jwt_secret: 'codeial',
+    morgan: {
+        mode: 'dev',
+        options: {stream: accessLogStream}
+    }
+}
+
+
+const production =  {
+    name: 'production',
+    asset_path: process.env.CODEIAL_ASSET_PATH,
+    session_cookie_key: process.env.CODEIAL_SESSION_COOKIE_KEY,
+    db: process.env.CODEIAL_DB,
+    smtp: {
+        service: 'gmail',
+        host: 'smtp.gmail.com',
+        port: 587,
+        secure: false,
+        auth: {
+            user: process.env.CODEIAL_GMAIL_USERNAME,
+            pass: process.env.CODEIAL_GMAIL_PASSWORD
+        }
+    },
+    google_client_id: process.env.CODEIAL_GOOGLE_CLIENT_ID,
+    google_client_secret: process.env.CODEIAL_GOOGLE_CLIENT_SECRET,
+    google_call_back_url: process.env.CODEIAL_GOOGLE_CALLBACK_URL,
+    jwt_secret: process.env.CODEIAL_JWT_SECRET,
+    morgan: {
+        mode: 'combined',
+        options: {stream: accessLogStream}
+    }
+}
+
+
+
+module.exports = eval(process.env.CODEIAL_ENVIRONMENT) == undefined ? development : eval(process.env.CODEIAL_ENVIRONMENT);
